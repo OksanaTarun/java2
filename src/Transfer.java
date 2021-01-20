eced3b86539fbdbc035770ad521e7735158375ff
OksanaTarun <oxana.tarun@gmail.com>
2021-01-20 10:16:17 +0300

    Initial commit

---
+++ src/Transfer.java
@@ -0,0 +1,9 @@
+public class Transfer {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int balance_2 = balance + transfer;
+        System.out.println(balance_2);
+    }
+}
+
